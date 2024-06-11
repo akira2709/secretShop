284d9cb18c831415260293f7fa3f3f8cd1d4e697
akira2709 <ivanlylin2009@gmail.com>
2024-06-11 17:40:20 +0300

    Update functions.js

--- src/functions.js
+++ src/functions.js
@@ -3,7 +3,7 @@
 export default function slash(value) {
   if (value > 99999){
     value = value.toExponential(1).replace('+', '')
-    return value
+    return value + 'hui'
   }
   return value
 }
